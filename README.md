a629d64ea13749a510ac3e847c6bb6d36ae254cc
Marlon271 <mdromero-2022b@corhuila.edu.co>
2024-10-30 17:53:03 -0500

    .

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # programacion-movil-c3-q2
 programacion-movil-c3-q2
+
+
+
+## Ejemplo de Código
+
+![paso 1](imagenes/1.png)
+![paso 2](imagenes/2.png)
+![paso 3](imagenes/3.png)
+![paso 4](imagenes/4.png)
+![paso 5](imagenes/5.png)
+![paso 6](imagenes/6.png)
+![paso 7](imagenes/7.png)
+![paso 7](imagenes/8.png)
+![paso 7](imagenes/9.png)
+![paso 7](imagenes/10.png)
+![paso 7](imagenes/11.png)
+![paso 7](imagenes/12.png)
---
+++ tienda/src/components/ProductInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- Componente de entrada de texto para el nombre del producto -->
+  <ion-item>
+    <ion-label position="stacked">{{ label }}</ion-label>
+    <ion-input v-model="value" :placeholder="placeholder"></ion-input>
+  </ion-item>
+</template>
+
+<script>
+export default {
+  name: 'ProductInput',
+  props: {
+    label: {
+      type: String,
+      default: 'Nombre del Producto'
+    },
+    placeholder: {
+      type: String,
+      default: 'Ingrese el nombre aquí...'
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+ion-item {
+  margin: 10px 0;
+}
+</style>
