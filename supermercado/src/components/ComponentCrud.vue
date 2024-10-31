ee6561d233b1e6a1b77c55aa113ec1ebc451d53e
Marlon271 <mdromero-2022b@corhuila.edu.co>
2024-10-31 11:57:43 -0500

    .

--- supermercado/src/components/ComponentCrud.vue
+++ supermercado/src/components/ComponentCrud.vue
@@ -7,39 +7,37 @@
     </ion-header>
 
     <ion-content class="ion-padding">
-      <ion-list>
+      <!-- Imagen de Encabezado -->
+      <div class="image-container">
+        <img :src="supermarketImage" alt="Supermercado" class="header-image" />
+      </div>
 
-        
+      <ion-list>
         <ion-item>
           <ion-label position="stacked">Producto</ion-label>
           <ion-input v-model="formData.productName" placeholder="Nombre del producto"></ion-input>
         </ion-item>
 
-      
         <ion-item>
           <ion-label position="stacked">SKU</ion-label>
           <ion-input v-model="formData.sku" placeholder="Código SKU"></ion-input>
         </ion-item>
 
-        
         <ion-item>
           <ion-label position="stacked">Stock</ion-label>
           <ion-input type="number" v-model="formData.stock" placeholder="Cantidad en stock"></ion-input>
         </ion-item>
 
-       
         <ion-item>
           <ion-label position="stacked">Precio (COP)</ion-label>
           <ion-input type="number" v-model="formData.price" placeholder="Precio en pesos"></ion-input>
         </ion-item>
 
-        
         <ion-item>
           <ion-label position="stacked">Descripción</ion-label>
           <ion-textarea v-model="formData.description" placeholder="Detalles del producto"></ion-textarea>
         </ion-item>
 
-       
         <ion-item>
           <ion-label position="stacked">Categoría</ion-label>
           <ion-select v-model="formData.category" placeholder="Seleccionar">
@@ -49,7 +47,6 @@
           </ion-select>
         </ion-item>
 
-        
         <ion-radio-group v-model="formData.status">
           <ion-list-header>
             <ion-label>Estado del Producto</ion-label>
@@ -64,15 +61,12 @@
           </ion-item>
         </ion-radio-group>
 
-        
         <ion-item>
-          <ion-label>Disponible en tienda</ion-label>
+          <ion-label>Disponible en supermercado</ion-label>
           <ion-toggle v-model="formData.isAvailable"></ion-toggle>
         </ion-item>
-
       </ion-list>
 
-      
       <div class="button-container">
         <ion-button expand="block" @click="handleSave">Guardar</ion-button>
         <ion-button expand="block" color="medium" @click="handleCancel">Cancelar</ion-button>
@@ -82,6 +76,8 @@
 </template>
 
 <script>
+import supermarketImage from '@/assets/supermarket.jpg'; 
+
 export default {
   name: 'ComponentCrud',
   data() {
@@ -95,7 +91,8 @@ export default {
         category: '',
         status: '',
         isAvailable: false,
-      }
+      },
+      supermarketImage, 
     };
   },
   methods: {
@@ -145,5 +142,18 @@ ion-item {
   gap: 10px;
   margin-top: 20px;
 }
+
+.image-container {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.header-image {
+  width: 100%;
+  max-width: 400px;
+  border-radius: 10px;
+}
 </style>
+
+
 
